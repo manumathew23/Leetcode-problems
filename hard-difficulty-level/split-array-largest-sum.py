94abb65b5b73a1e01bac0026e2e3bd5745234151
Manu Mathew <manu_mathew@outlook.com>
2022-03-31 12:46:53 +0530

    Update and rename hard-difficulty-level.py to hard-difficulty-level/split-array-largest-sum.py

--- hard-difficulty-level/split-array-largest-sum.py
+++ hard-difficulty-level/split-array-largest-sum.py
@@ -50,3 +50,23 @@ class Solution:
             if cnt>m: lo = mid+1
             else: hi = mid
         return hi
+
+''' Solution explanation
+
+The minimum value of sum cannot be lower than the largest element of the array nums . This is because even if we split all element into group of size 1, largest element will have the largest sum.
+
+So, this value will be the staring range of Binary Search.
+
+e.g. if nums = [7,2,5,10,8], then sum cannot be lower that 10.
+
+Similarly, the largest possible value of Sum will be when we don't divide the array at all and all elements are in the same group.
+
+Ending range of Binary Search will be the sum of the whole array nums.
+
+We will then calculate mid value of this range and check if We can have atmost m subarray groups, such that the sum of each group does not exceed mid.
+
+To do this, we can take a variable count to store the number of paritions needed so that the maximum sum of the nums does not exceed mid.
+
+If the total number of groups after partition is less than the m, we can split the any of the current group of size >=2.
+This will not increase the maximum sum and hence our answer will still be valid.
+'''
